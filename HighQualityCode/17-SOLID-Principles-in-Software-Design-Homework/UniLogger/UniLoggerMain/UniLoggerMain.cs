7ed55575665b8f54f30e40c93e09be5f96f0e755
borko9696 <borko9696@mail.bg>
2016-02-17 09:47:48 +0200

    Add SOLID Principles in Software Design Homework

---
+++ HighQualityCode/17-SOLID-Principles-in-Software-Design-Homework/UniLogger/UniLoggerMain/UniLoggerMain.cs
@@ -0,0 +1,23 @@
+﻿namespace UniLoggerMain
+{
+    using UniLogger.Appenders;
+    using UniLogger.Contracts;
+    using UniLogger.Layouts;
+    using UniLogger.Logger;
+
+    internal class UniLoggerMain
+    {
+        public static void Main()
+        {
+            var simpleLayout = new SimpleLayout();
+
+            var consoleAppender = new ConsoleAppender(simpleLayout);
+            var fileAppender = new FileAppender(simpleLayout) { File = "log.txt" };
+
+            var logger = new Logger(consoleAppender, fileAppender);
+            logger.Error("Error parsing JSON.");
+            logger.Info(string.Format("User {0} successfully registered.", "Pesho"));
+            logger.Warn("Warning - missing files.");
+        }
+    }
+}
