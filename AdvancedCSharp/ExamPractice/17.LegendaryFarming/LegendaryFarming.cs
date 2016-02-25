d72431618154b063a01ad38fb5395f7bacd0626c
borko9696 <borko9696@mail.bg>
2016-02-25 09:15:57 +0200

    Exam preparation 24.02.2016

---
+++ AdvancedCSharp/ExamPractice/17.LegendaryFarming/LegendaryFarming.cs
@@ -0,0 +1,74 @@
+﻿namespace _04.LegendaryFarming
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class LegendaryFarming
+    {
+        private static void Main()
+        {
+            var junk = new SortedDictionary<string, int>();
+            var keyMats = new Dictionary<string, int>();
+            keyMats.Add("shards", 0);
+            keyMats.Add("motes", 0);
+            keyMats.Add("fragments", 0);
+
+            var keyWinner = string.Empty;
+
+            while (keyWinner == String.Empty)
+            {
+                var input = Console.ReadLine().Split(' ');
+
+                for (int i = 0; i < input.Length; i+=2)
+                {
+                    var material = input[i + 1].ToLower();
+                    var quantity = int.Parse(input[i]);
+
+                    if (!keyMats.ContainsKey(material))
+                    {
+                        if (!junk.ContainsKey(material))
+                        {
+                            junk[material] = 0;
+                        }
+                        junk[material] += quantity;
+                    }
+                    else
+                    {
+                        keyMats[material] += quantity;
+                        if (keyMats[material] >= 250)
+                        {
+                            keyMats[material] -= 250;
+                            keyWinner += material;
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            if (keyWinner.Equals("motes"))
+            {
+                Console.WriteLine("Dragonwrath obtained!");
+            }
+            else if (keyWinner.Equals("fragments"))
+            {
+                Console.WriteLine("Valanyr obtained!");
+            }
+            else
+            {
+                Console.WriteLine("Shadowmourne obtained!");
+            }
+
+            var newKeyMats = keyMats.OrderByDescending(x => x.Value);
+            foreach (var pair in newKeyMats)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key , pair.Value);
+            }
+            foreach (var pair in junk)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
---
+++ AdvancedCSharp/ExamPractice/18.OlympicsAreComing/OlympicsAreComing.cs
@@ -0,0 +1,80 @@
+﻿namespace _04.OlympicsAreComing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class OlympicsAreComing
+    {
+        private static void Main()
+        {
+            var input = Console.ReadLine();
+            ;
+
+            var data = new Dictionary<string, Dictionary<string, int>>();
+
+            while (input != "report")
+            {
+                var inputArray = input.Split('|');
+
+                var player = string.Empty;
+                player = FormatPlayer(inputArray, player);
+                
+                var country = string.Empty;
+                country = FormatCountry(inputArray, country);
+
+                FieldTheDictionary(data, country, player);
+
+                input = Console.ReadLine();
+            }
+
+            var newDict = data.OrderByDescending(x => x.Value.Sum(y => y.Value));
+            foreach (var pair in newDict)
+            {
+                var dict = pair.Value;
+                Console.WriteLine("{0} ({1} participants): {2} wins", pair.Key, dict.Count, pair.Value.Sum(x => x.Value));
+            }
+        }
+
+        private static void FieldTheDictionary(Dictionary<string, Dictionary<string, int>> data, string country, string player)
+        {
+            if (!data.ContainsKey(country))
+            {
+                data[country] = new Dictionary<string, int>();
+            }
+            if (!data[country].ContainsKey(player))
+            {
+                data[country][player] = 0;
+            }
+            data[country][player] += 1;
+        }
+
+        private static string FormatCountry(string[] inputArray, string country)
+        {
+            if (inputArray[1].Trim().Contains(' '))
+            {
+                var nonFormatedCountry = inputArray[1].Trim().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                country = nonFormatedCountry[0].Trim() + " " + nonFormatedCountry[1].Trim();
+            }
+            else
+            {
+                country = inputArray[1].Trim();
+            }
+            return country;
+        }
+
+        private static string FormatPlayer(string[] inputArray, string player)
+        {
+            if (inputArray[0].Trim().Contains(' '))
+            {
+                var nonFormatPlayer = inputArray[0].Trim().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                player = nonFormatPlayer[0].Trim() + " " + nonFormatPlayer[1].Trim();
+            }
+            else
+            {
+                player = inputArray[0].Trim();
+            }
+            return player;
+        }
+    }
+}
