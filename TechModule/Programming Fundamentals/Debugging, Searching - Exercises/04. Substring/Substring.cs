d9249f1526a1ca976824cfe2a65b50f7bbac7110
borko9696 <borko9696@mail.bg>
2016-05-18 10:04:15 +0300

    Solved Git, GitHub, Debugging, Searching - Lab Solved Git, GitHub, Debugging, Searching - Exercises

---
+++ TechModule/Programming Fundamentals/Debugging, Searching - Exercises/04. Substring/Substring.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Substring
+{
+    class Substring
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+
+            const char Search = 'p';
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var currentChar = text[i];
+                if (currentChar == Search)
+                {
+                    hasMatch = true;
+
+                    int end = jump + 1;
+
+                    if (end + i > text.Length)
+                    {
+                        end = text.Length - i;
+                    }
+
+                    string matchedString = text.Substring(i, end);
+                    Console.WriteLine(matchedString);
+                    i += jump;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
+
