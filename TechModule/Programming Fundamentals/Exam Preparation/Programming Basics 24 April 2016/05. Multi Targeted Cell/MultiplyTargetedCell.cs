8ff879f1d5b67aed755cfe2a52929ab47f628b35
borko9696 <borko9696@mail.bg>
2016-06-03 21:17:39 +0300

    Solved 5/5 Programming Basics 24 April 2016

---
+++ TechModule/Programming Fundamentals/Exam Preparation/Programming Basics 24 April 2016/05. Multi Targeted Cell/MultiplyTargetedCell.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Multi_Targeted_Cell
+{
+    class MultiplyTargetedCell
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var row = size[0];
+            var col = size[1];
+
+            var matrix = new long[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                long[] line = Console.ReadLine().Split().Select(long.Parse).ToArray();
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            var center = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var centerValue = matrix[center[0], center[1]];
+            long sum = -centerValue;
+
+            for (int i = center[0] - 1; i <= center[0] + 1; i++)
+            {
+                for (int j = center[1] - 1; j <= center[1] + 1; j++)
+                {
+                    try
+                    {
+                        sum += matrix[i, j];
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+            }
+
+            for (int i = center[0] - 1; i <= center[0] + 1; i++)
+            {
+                for (int j = center[1] - 1; j <= center[1] + 1; j++)
+                {
+                    try
+                    {
+                        long temp = matrix[i, j];
+                        if (i == center[0] && j == center[1])
+                        {
+                            matrix[i, j] = temp * sum;
+                        }
+                        else
+                        {
+                            matrix[i, j] = centerValue * temp;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(matrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TechModule/Programming Fundamentals/Exam Preparation/Programming Basics 24 April 2016/Problem 4.Array Modifier/ArrayModifier .cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4.Array_Modifier
+{
+    public class ArrayModifier
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            var commands = new List<string>();
+
+            var inputCommand = Console.ReadLine();
+            while (inputCommand != "end")
+            {
+                commands.Add(inputCommand);
+                inputCommand = Console.ReadLine();
+            }
+
+            for (int i = 0; i < commands.Count; i++)
+            {
+                var currentCommand = commands[i].Split();
+                switch (currentCommand[0])
+                {
+                    case "swap":
+                        Swap(numbers, long.Parse(currentCommand[1]), long.Parse(currentCommand[2]));
+                        break;
+                    case "multiply":
+                        Multiply(numbers, long.Parse(currentCommand[1]), long.Parse(currentCommand[2]));
+                        break;
+                    case "decrease":
+                        numbers = Decrease(numbers);
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static long[] Decrease(long[] numbers)
+        {
+            var array = numbers.Select(x => x - 1).ToArray();
+            return array;
+        }
+
+        private static void Multiply(long[] numbers, long firstIndex, long secondIndex)
+        {
+            long temp = numbers[firstIndex];
+            numbers[firstIndex] = temp * numbers[secondIndex];
+        }
+
+        private static void Swap(long[] numbers, long firstIndex, long secondIndex)
+        {
+            long temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
+        }
+    }
+}
