e6451230bd0c70ede64f624672436a9f01f17338
borko9696 <borko9696@mail.bg>
2016-06-24 23:15:07 +0300

    Solve JQuery and Ajax - Lab

---
+++ AdvancedCSharp/ExamPractice/26. Cubic Assault/CubicAssault.cs
@@ -0,0 +1,95 @@
+﻿namespace _26.Cubic_Assault
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text.RegularExpressions;
+
+    internal class CubicAssault
+    {
+        private static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, Dictionary<string, BigInteger>>();
+
+            var input = Console.ReadLine();
+            while (input != "Count em all")
+            {
+                var arguments = Regex.Split(input, "->").Select(x => x.Trim()).ToArray();
+                var region = arguments[0];
+                var type = arguments[1];
+                var value = BigInteger.Parse(arguments[2]);
+
+                if (!dict.ContainsKey(region))
+                {
+                    dict[region] = new Dictionary<string, BigInteger>();
+                    dict[region]["Black"] = 0;
+                    dict[region]["Red"] = 0;
+                    dict[region]["Green"] = 0;
+                    dict[region][type] = value;
+                }
+                else
+                {
+                    var oldValue = dict[region][type];
+                    dict[region][type] = oldValue + value;
+                }
+
+                switch (type)
+                {
+                    case "Black":
+                        break;
+                    case "Red":
+                        if (dict[region][type] >= 1000000)
+                        {
+                            while (dict[region][type] >= 1000000)
+                            {
+                                dict[region]["Black"]++;
+                                dict[region][type] -= 1000000;
+                            }
+
+                            dict[region][type] = 0;
+                        }
+
+                        break;
+                    case "Green":
+                        if (dict[region][type] >= 1000000)
+                        {
+                            while (dict[region][type] >= 1000000)
+                            {
+                                dict[region]["Red"]++;
+                                dict[region][type] -= 1000000;
+                            }
+
+                            dict[region][type] = 0;
+                            if (dict[region]["Red"] >= 1000000)
+                            {
+                                while (dict[region]["Red"] >= 1000000)
+                                {
+                                    dict[region]["Black"]++;
+                                    dict[region]["Red"] -= 1000000;
+                                }
+
+                                dict[region]["Red"] = 0;
+                            }
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var result = dict.OrderByDescending(x => x.Value["Black"]).ThenBy(x => x.Key.Length).ThenBy(x => x.Key);
+            foreach (var pair in result)
+            {
+                Console.WriteLine(pair.Key);
+                var values = pair.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+                foreach (var value in values)
+                {
+                    Console.WriteLine("-> {0} : {1}", value.Key, value.Value);
+                }
+            }
+        }
+    }
+}
---
+++ AdvancedCSharp/ExamPractice/27.Cars/Cars.cs
@@ -0,0 +1,121 @@
+﻿namespace _27.Cars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string model = input[0];
+
+                var engine = new Engine(int.Parse(input[1]), int.Parse(input[2]));
+
+                var cargo = new Cargo(int.Parse(input[3]), (CargoType)Enum.Parse(typeof(CargoType), input[4]));
+
+                var tire =
+                    new Tire(
+                        new List<double>
+                            {
+                                double.Parse(input[5]),
+                                double.Parse(input[7]),
+                                double.Parse(input[9]),
+                                double.Parse(input[11])
+                            },
+                        new List<int>
+                            {
+                                int.Parse(input[6]),
+                                int.Parse(input[8]),
+                                int.Parse(input[10]),
+                                int.Parse(input[12])
+                            });
+             
+
+
+                var car = new Car(model, engine, cargo, tire);
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                var newCars =
+                    cars.Where(x => x.Cargo.CargoType == CargoType.fragile)
+                        .Where(y => y.Tire.TireOnePreassure.Exists(t => t < 1d))
+                        .ToList();
+            }
+        }
+    }
+
+    public class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo, Tire tires)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.Tire = tires;
+        }
+
+        public Tire Tire { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public string Model { get; set; }
+    }
+
+    public class Engine
+    {
+        public Engine(int engineSpeed, int enginePower)
+        {
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+        }
+
+        public int EnginePower { get; set; }
+
+        public int EngineSpeed { get; set; }
+    }
+
+    public class Cargo
+    {
+        public Cargo(int cargoWeight, CargoType cargoType)
+        {
+            this.CargoWeight = cargoWeight;
+            this.CargoType = cargoType;
+        }
+
+        public CargoType CargoType { get; set; }
+
+        public int CargoWeight { get; set; }
+    }
+
+    public class Tire
+    {
+        public Tire(List<double> tireOnePreassure, List<int> tireOneAge)
+        {
+            this.TireOnePreassure = tireOnePreassure;
+            this.TireOneAge = tireOneAge;
+        }
+
+        public List<double> TireOnePreassure { get; set; }
+
+        public List<int> TireOneAge { get; set; }
+    }
+
+    public enum CargoType
+    {
+        fragile,
+        flamable
+    }
+}
