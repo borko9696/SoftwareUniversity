e1d67c272d9da08564087be05517fadd790befa0
borko9696 <borko9696@mail.bg>
2016-06-28 20:33:53 +0300

    Solve Lab PHP First Steps

---
+++ TechModule/Software Technologies/Lab PHP First Steps/06. Sum-Two-Numbers.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="bg">
+<head>
+    <meta charset="UTF-8">
+    <title>Sum Two Numbers</title>
+</head>
+<body>
+<?php if (isset($_GET['num1']) && isset($_GET['num2'])) {
+    $firstNum = $_GET['num1'];
+    $secondNum = $_GET['num2'];
+    $result = $firstNum + $secondNum;
+    echo "$firstNum + $secondNum = $result";
+}
+?>
+
+<form>
+    <div>First Number:</div>
+    <input type="number" name="num1" />
+    <div>Second Number:</div>
+    <input type="number" name="num2" />
+    <div><input type="submit" /></div>
+</form>
+</body>
+</html>
