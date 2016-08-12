87e60c7cee3e6d1db242b0df3a02bc48c3dc181c
borko9696 <borko9696@mail.bg>
2016-08-13 00:22:51 +0300

    MVC Exercises MVC Fixed

---
+++ TechModule/Software Technologies/Exercises MVC/WebAppExercise/WebAppExercise/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppExercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> items = (List<string>)this.Session["item"] ?? new List<string>();
+            return View(items);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddItem(string newItem)
+        {
+            List<string> items = (List<string>)this.Session["item"] ?? new List<string>();
+            items.Add(newItem);
+            this.Session["item"] = items;
+            return this.RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveItem(int index)
+        {
+            List<string> items = (List<string>)this.Session["item"];
+
+            if (items != null && index < items.Count)
+            {
+                items.RemoveAt(index);
+                this.Session["Items"] = items;
+            }
+
+            return this.RedirectToAction("Index");
+        }
+    }
+}
