3ab58527d579702185902f44acfff128dfb2334b
borko9696 <borko9696@mail.bg>
2016-11-09 20:22:45 +0200

    Solved 01. Introduction to DB Apps Exercises, 03. Introduction to EntityFramework Exercises, 04. EntityFramework Code-First Exercises

---
+++ TechModule/Software Technologies/MVCBlog/MVCBlog/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    using MVCBlog.Models;
+
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(3);
+
+            return View(posts.ToList());
+        }
+
+    }
+}
---
+++ TechModule/Software Technologies/MVCBlog/MVCBlog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
